cf60e729db5d5cc41d244d711b49747db4a0041c
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2023-03-23 16:50:13 +0000

    going back

---
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() type = 'text';
+  @Input() label = '';
+
+  //@Self to guarantee that each time we use this component its a new un-used input
+  constructor(@Self() public controlDir: NgControl){
+    this.controlDir.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl {
+    //need to set the controlDr as a form control so we can use it in our template
+    return this.controlDir.control as FormControl
+  }
+
+
+}
