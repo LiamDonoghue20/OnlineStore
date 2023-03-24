2a16eb5a782353561c207900e755495ab2c108af
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2023-03-24 20:17:20 +0000

    adding validation to api requests

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,10 +1,20 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", ErrorMessage =
+        "Password must contain atleast 1 Uppercase letter, 1 Lowercase letter, 1 Number and 1 special character")]
         public string Password { get; set; }
     }
 }
