a3c4197c4e951b4b6af01531a6c717c1b0f2f904
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2023-04-10 14:18:33 +0100

    end of section 19

---
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/account/account.service';
+
+@Component({
+  selector: 'app-checkout-address',
+  templateUrl: './checkout-address.component.html',
+  styleUrls: ['./checkout-address.component.scss']
+})
+export class CheckoutAddressComponent {
+  @Input() checkoutForm? : FormGroup;
+
+  constructor(private accountService: AccountService, private toastr: ToastrService){}
+
+  saveUserAddress(){
+    this.accountService.updateUserAddress(this.checkoutForm?.get('addressForm')?.value).subscribe({
+      next: () => {
+        this.toastr.success('Address saved');
+        //retain the new address details that were input into the form so they aren't wiped when submitted
+        this.checkoutForm?.get('addressForm')?.reset(this.checkoutForm?.get('addressForm')?.value);
+      }
+    })
+  }
+
+}
